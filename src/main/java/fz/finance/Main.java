d6023927aa2b781665b13ecd932ca77949556304
efzeti <00franio@gmail.com>
2021-02-22 14:32:49 +0100

    Initial commit

---
+++ src/main/java/fz/finance/Main.java
@@ -0,0 +1,270 @@
+package fz.finance;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.histquotes.Interval;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Main {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String STKS_PATH = "src\\main\\resources\\stk.txt";
+
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
+
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static int counter = 1;
+
+    public static void main(String[] args) throws IOException {
+
+
+
+        Calendar dummyFrom = Calendar.getInstance();
+        Stock stock = YahooFinance.get("TSLA", dummyFrom);
+        stock.print();
+        System.out.println(DATE_FORMAT.format(stock.getHistory().get(0).getDate().getTime()));
+        System.out.println(stock.getHistory().get(0).getClose().doubleValue() );
+//        Calendar from = Calendar.getInstance();
+//        Calendar smallFrom = Calendar.getInstance();
+//        Calendar to = Calendar.getInstance();
+//
+//        int bigSMADays = 200;
+//        int smolSMADays = 50;
+//
+//        from.add(Calendar.DAY_OF_MONTH, -2 * bigSMADays);
+//        smallFrom.add(Calendar.DAY_OF_MONTH, -2 * smolSMADays);
+//
+//        System.out.println(DATE_FORMAT.format(from.getTime()));
+//        System.out.println(DATE_FORMAT.format(to.getTime()));
+//
+//        Stock google = YahooFinance.get("TSLA", from, to, Interval.DAILY);
+//
+//        List<HistoricalQuote> gogHist = getHistoricalData("TSLA", 200);
+//
+//
+//
+//        System.out.println(DATE_FORMAT.format(gogHist.get(0).getDate().getTime()));
+//        System.out.println(gogHist.get(0).getClose());
+//
+//        double bigSMACounter = 0;
+//        double smolSMACounter = 0;
+//
+//        int smolCounter = 0;
+//        int bigCounter = 0;
+//
+//        for (HistoricalQuote histQuote : gogHist){
+//            bigSMACounter = bigSMACounter + histQuote.getClose().doubleValue();
+//            bigCounter++;
+//            if (smolCounter<smolSMADays){
+//                smolSMACounter = smolSMACounter + histQuote.getClose().doubleValue();
+//                smolCounter++;
+//                System.out.println(DATE_FORMAT.format(histQuote.getDate().getTime()));
+//            }
+//
+//            if (bigCounter >= bigSMADays){
+//                break;
+//            }
+//        }
+//
+//        System.out.println(bigCounter);
+//        System.out.println(smolCounter);
+//
+//        double bigSMA = bigSMACounter / bigCounter;
+//        double smolSMA = smolSMACounter / smolCounter;
+//
+//        System.out.println("bigSMA: " + bigSMA);
+//        System.out.println("smolSMA: " + smolSMA);
+//
+//        countSMA(gogHist, 200, false);
+//        countSMA(gogHist, 50, false);
+//
+//        crossCalculator("TSLA",50,200);
+
+
+        List<String>stks = makeStkListFromFile(STKS_PATH);
+
+        stks.parallelStream().forEach( stk -> {
+
+            crossCalculator(stk, 15, 50);
+            System.out.println("STK Symbol: " + stk + " Counter: " + counter++);
+
+           });
+
+
+
+
+
+    }
+
+    public static List<String> makeStkListFromFile(String filePath){
+
+        List<String> stkList = new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+
+            String stkLine = br.readLine();
+            while(stkLine != null){
+                stkList.add(stkLine);
+                stkLine = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return stkList;
+
+    }
+
+    public static void printStks(List<String> stks){
+        List<String> wrongStks = new ArrayList<>();
+
+        stks.parallelStream().forEach( stk -> {
+
+            Stock streamStock = null;
+
+            try {
+                streamStock = YahooFinance.get(stk);
+            } catch (IOException ex) {
+                System.out.println(ANSI_RED + "Cannot initialize: " + stk + ANSI_RESET);
+            }
+
+            if (streamStock != null){
+                if (streamStock.getQuote().getPrice() != null){
+                    BigDecimal streamPrice = streamStock.getQuote().getPrice();
+
+                    System.out.format("%-5s is valued at " + streamPrice + "$\n", stk);
+                } else {
+                    System.out.println(ANSI_RED + stk + " initialized properly but the price is null." + ANSI_RESET);
+                    wrongStks.add(stk);
+                }
+
+            } else {
+                wrongStks.add(stk);
+            }});
+
+        System.out.println(ANSI_RED + "Wrong stocks: " + wrongStks + ANSI_RESET);
+    }
+
+    public static double countSMA(String stkSymbol, int SMADays, boolean dayMinus){
+        List<HistoricalQuote> histData = getHistoricalData(stkSymbol, SMADays);
+        return countSMA(histData, SMADays, dayMinus);
+    }
+
+    public static double countSMA(List<HistoricalQuote> histData, int SMADays, boolean dayMinus){
+
+        if (histData == null){
+            return -1D;
+        }
+
+        while (histData.get(0).getDate().getTime().before(histData.get(1).getDate().getTime())){
+            System.out.println("Reversing historical data list.");
+            Collections.reverse(histData);
+        }
+
+        double SMACounter = 0;
+        int loopCounter = 0;
+
+
+        for (int i = (dayMinus ? 1 : 0); i < SMADays + (dayMinus ? 1 : 0); i++){
+            SMACounter = SMACounter + histData.get(i).getClose().doubleValue();
+            loopCounter++;
+        }
+
+        double SMA = SMACounter / loopCounter;
+//        System.out.format("Counted %d entries, SMA is: %.2f$\n", loopCounter, SMA);
+
+        return SMA;
+
+    }
+
+    public static void crossCalculator(String stkSymbol, int smolSMADays, int bigSMADays){
+
+        List<HistoricalQuote> histData = getHistoricalData(stkSymbol, bigSMADays);
+
+
+
+        double smolSMA = countSMA(histData, smolSMADays, false);
+        double smolSMAMinusDay = countSMA(histData, smolSMADays, true);
+        double bigSMA = countSMA(histData, bigSMADays, false);
+        double bigSMAMinusDay = countSMA(histData, bigSMADays, true);
+
+//        System.out.println(smolSMA);
+//        System.out.println(smolSMAMinusDay);
+//        System.out.println(bigSMA);
+//        System.out.println(bigSMAMinusDay);
+
+        if ((smolSMA > bigSMA) && (smolSMAMinusDay < bigSMAMinusDay)){
+            System.out.format(ANSI_GREEN + "%s has made a golden cross\n" + ANSI_RESET, stkSymbol);
+            System.out.printf("(%.2f > %.2f) && (%.2f < %.2f)\n", smolSMA, bigSMA, smolSMAMinusDay, bigSMAMinusDay);
+        } if ((smolSMA < bigSMA) && (smolSMAMinusDay > bigSMAMinusDay)){
+            System.out.format(ANSI_PURPLE + "%s has made a death cross\n" + ANSI_RESET, stkSymbol);
+            System.out.printf("(%.2f < %.2f) && (%.2f > %.2f)\n", smolSMA, bigSMA, smolSMAMinusDay, bigSMAMinusDay);
+        }
+
+
+    }
+
+    public static List<HistoricalQuote> getHistoricalData(String stkSymbol, int daysPast){
+
+        Calendar from = Calendar.getInstance();
+        Calendar to = Calendar.getInstance();
+
+        from.add(Calendar.DAY_OF_MONTH, -2 * daysPast);
+
+//        System.out.println(DATE_FORMAT.format(from.getTime()));
+//        System.out.println(DATE_FORMAT.format(to.getTime()));
+
+        Stock stock = null;
+        try {
+            stock = YahooFinance.get(stkSymbol, from, to, Interval.DAILY);
+        } catch (IOException e) {
+            System.out.println(ANSI_RED + stkSymbol + " couldn't be initialized." + ANSI_RESET);
+            return null;
+        }
+
+        List<HistoricalQuote> histData = null;
+        try {
+            histData = stock.getHistory();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e2){
+            System.out.println(ANSI_RED +"History for " + stkSymbol + " couldn't be initialized." + ANSI_RESET);
+            return null;
+        }
+
+        Collections.reverse(histData);
+
+        if (histData.size() < daysPast + 1){
+            System.out.println(ANSI_RED + stkSymbol + " is too fresh company to be considered in this timeframe." + ANSI_RESET);
+            return null;
+        }
+
+        List<HistoricalQuote> readyList = List.copyOf(histData.subList(0, daysPast + 1));
+
+        return readyList;
+
+    }
+
+}
+
+
